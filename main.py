a6d64e5811c79bda3a2967cdab8f68108880231e
Gordoquis <vitor.raimundo@aluno.senai.br>
2026-03-06 10:12:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 @app.route("/")
-
-
 def index():
-    return "<h1>Hello World!</h1>"
+    return "<h1>Hello World!!</h1>"
 
-if __name__=="__main__":
+@app.route("/curso")
+def index1():
+    return "<h1>Meu curso é Desenvolvimento de Sistemas</h1>"
+
+if __name__ == "__main__":
     app.run()
